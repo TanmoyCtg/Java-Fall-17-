3ced98319acc08face4fdbba22e93225e607f0ec
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2017-11-18 23:31:20 +0600

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,10 +1,13 @@
+//create Person classes
 public class Person{
-String name;
-int age;
-
+// name and age is variable
+  String name;
+  int age;
+// main is method 
+// everything starts from main
 public static void main(String[] args){
-  Person joy = new Person(); 
-  System.out.println(joy.name);
-  System.out.println(joy.age);
+  Person joy = new Person();  // create joy objects.
+  System.out.println(joy.name); // joy objects access the name variable
+  System.out.println(joy.age); //// joy objects access the age variable
 }
 }

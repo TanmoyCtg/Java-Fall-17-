f50567d554cea23584ef5650ff9671791d15243e
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2017-11-18 23:28:19 +0600

    This is the my documentation code
    i will mention Exercise1 in my documentation.  so this is the first code. sorry for indentation problem in doc.

---
+++ Exercise1.java
@@ -0,0 +1,10 @@
+public class Person{
+String name;
+int age;
+
+public static void main(String[] args){
+  Person joy = new Person(); 
+  System.out.println(joy.name);
+  System.out.println(joy.age);
+}
+}

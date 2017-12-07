0fd61198b4cdeb93c9d9830a2a37fd09c0c3bc3a
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-07 21:35:06 +0600

    add new function

--- src/polymorphism/src/Employee.java
+++ src/polymorphism/src/Employee.java
@@ -1,5 +1,46 @@
 
 // Employee is abstract super class
 public abstract class Employee {
-    
-}
+    // declare three private variable
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    // declare a constructor
+    public Employee(String first, String last, String ssn) {
+        firstName = first;
+        lastName = last;
+        socialSecurityNumber = ssn;
+    }
+
+    // set the employee first name
+    void setFirstName(String first) {
+        firstName = first;
+    }
+
+    // return the get name
+    String getFirstName() {
+        return firstName;
+    }
+
+    void setLastName(String last){
+        lastName = last;
+    }
+    String getLastName(){
+        return lastName;
+    }
+
+    void setSocialSecurityNumber(String ssn){
+        socialSecurityNumber = ssn;
+    }
+    String getSocialSecurityNumber(){
+        return socialSecurityNumber;
+    }
+    // return or represent nicely representation employee object
+    public String toString(){
+        return String.format("%s %s\nsocial security number: %s",getFirstName(),getLastName(),getSocialSecurityNumber());
+
+    }
+   // abstract method overridden by subclasses
+   public  abstract double earnings(); // no implementation here
+} // end abstract class employee

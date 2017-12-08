9da625f01ff5d9ceb2a2e8d63ed507dec86bb762
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-08 20:39:08 +0600

    Add hourlyemployee

---
+++ src/polymorphism/src/HourlyEmployee.java
@@ -0,0 +1,33 @@
+public class HourlyEmployee extends Employee {
+    private double wage;
+    private int Hours;
+    public HourlyEmployee(String first, String last, String ssn, double hourlyWage,int hours){
+        super(first,last,ssn);
+
+    }
+    // set wage
+    public void setWage(double hourlyWage){
+        wage = (hourlyWage <0.0 )? 0.0 : hourlyWage;
+    }
+    public double getWage(){
+        return wage;
+    }
+    public void setHours(int hours){
+        Hours = ((hours >= 0) && (hours <=168)) ? hours:0;
+    }
+
+    public int getHours(){
+        return Hours;
+    }
+
+    public double earnings(){
+        if (getHours() <= 40) // no overtime
+            return getWage() * getHours();
+        else
+            return 40 * getWage() * (getHours() - 40) * getWage() * 1.5;
+    }
+    public String toString(){
+        return String.format("hourly employee: %s\n%s: $%,.2f; %s:%,.2f","hours worked",getHours()
+        ,super.toString(),"hourly wage",getWage());
+    }
+}

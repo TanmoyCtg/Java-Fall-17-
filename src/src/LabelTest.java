d7fc9b4720fe8a4071f62e4e93d023e1802c21f9
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-14 03:22:54 +0600

    write the label

---
+++ src/src/LabelTest.java
@@ -0,0 +1,9 @@
+import java.net.MalformedURLException;
+
+public class LabelTest {
+    public static void main(String[] args) throws MalformedURLException {
+        LabelFrame lbl = new LabelFrame();
+        lbl.setSize(1400,1300);
+        lbl.setVisible(true);
+    }
+}

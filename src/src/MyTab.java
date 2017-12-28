916a214d72a7bc65077bf17a0ad4316fb3b13e17
DESKTOP-BE3CB0C\chowd <iftekharjoy1@gmail.com>
2017-12-28 23:33:11 +0600

    initial commit

--- src/src/MyTab.java
+++ src/src/MyTab.java
@@ -1,9 +1,1 @@
-import javax.swing.*;
 
-public class MyTab extends JFrame{
-    public MyTab(){
-        JTabbedPane tab = new JTabbedPane(JTabbedPane.CENTER);
-        tab.addTab("Tab1");
-    }
-
-}
--- src/src/MyTabMain.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-public class MyTabMain  {
-    public static void main(String[] args){
-        MyTab tab = new MyTab();
-
-        //tab.setSize(300,200);
-    }
-}
